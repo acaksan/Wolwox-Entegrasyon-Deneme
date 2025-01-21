43922b64dd022faa5026a68a3a822963009d8c10
acaksan <a.c.aksan@gmail.com>
2025-01-21 18:45:00 +0300

    Clean start with basic Vue.js and Flask setup

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, 
+    static_folder='static',
+    template_folder='templates'
+)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
